f92e9eb3d5dc6ec0f5f298f242e866a5e2a81660
Mitchell Sayre <sayremitch@gmail.com>
2025-01-18 00:40:24 -0500

    Fix default values

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,8 +5,8 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Wfloat Documentation',
+  tagline: 'Realistic computer voice that runs embedded in your app.',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -96,7 +96,7 @@ const config: Config = {
           position: 'right',
         },
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/wfloat/docs',
           label: 'GitHub',
           position: 'right',
         },
